b7b8793a4cd017109921c7af34b8e18789817931
m_axwel_l <shaxa882@gmail.com>
2023-03-30 15:20:37 +0500

    fixed

---
+++ ProductTotalTest/ProductTotalTest.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Configuration;
+using TestProject.Domain.Products;
+using TestProject.Domain.Products.Profiles;
+
+namespace ProductTotalTest;
+
+public class ProductTotalTest 
+{
+    IMapper mapper;
+
+    public ProductTotalTest()
+    {
+        this.mapper = new ProductProfile(9).GetMapper();
+    }
+
+    [Fact]
+    public void TotalPriceWithVat()
+    {
+        // VAT = 9
+        var productShow = this.mapper.Map<ProductShow>(new Product()
+        {
+            Price = 10,
+            Quantity = 10,
+        });
+        Assert.True(productShow.TotalPriceWithVTO == 1000);
+    }
+}
